d574b75e862fbc9fd57e739e389a7c4f82ad9297
Anisha Sharma <anishasharma@Anishas-MacBook-Pro.local>
2022-11-12 15:09:54 -0500

    Flask-Auth

--- flask_sample/main.py
+++ flask_sample/main.py
@@ -1,15 +1,48 @@
+# from https://towardsdatascience.com/deploy-to-google-cloud-run-using-github-actions-590ecf957af0
 import os
+import sys
+from flask import Flask
+from dotenv import load_dotenv
+load_dotenv()
+import flask_login
+# added so modules can be found between the two different lookup states:
+# from tests and from regular running of the app
+CURR_DIR = os.path.dirname(os.path.abspath(__file__))
+print(CURR_DIR)
+sys.path.append(CURR_DIR)
 
-from flask import Flask, request
+login_manager = flask_login.LoginManager()
+# app = Flask(__name__)
+def create_app(config_filename=''):
+    app = Flask(__name__)
+    app.secret_key = os.environ.get("SECRET_KEY", "missing_secret")
+    login_manager.init_app(app)
+    # app.config.from_pyfile(config_filename)
+    with app.app_context():
+        from views.hello import hello
+        app.register_blueprint(hello)
+        from views.sample import sample
+        app.register_blueprint(sample)
+        from auth.auth import auth
+        app.register_blueprint(auth)
+        return app
 
-app = Flask(__name__)
 
+@login_manager.user_loader
+def load_user(user_id):
+    from sql.db import DB
+    from auth.models import User
+    try:
+        print("login_manager loading user")
+        result = DB.selectOne("SELECT id, email FROM IS601_Users WHERE id = %s", user_id)
+        if result.status:
+            return User(**result.row)
+    except Exception as e:
+        print(e)
+    return None
 
-@app.route('/')
-def hello_world():
-    name = request.args.get('name', 'World')
-    return f'Hello {name}!'
+app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
